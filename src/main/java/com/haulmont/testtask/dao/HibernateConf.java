a1c758942a9d1539e5e986b2f755654da253757d
gzem2 <gzem2@yandex.ru>
2021-07-03 22:13:14 +0400

    Fix HibernateConf

--- src/main/java/com/haulmont/testtask/dao/HibernateConf.java
+++ src/main/java/com/haulmont/testtask/dao/HibernateConf.java
@@ -4,15 +4,15 @@ import java.util.Properties;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
 public class HibernateConf {
+    @Primary
     @Bean(name="entityManagerFactory")
     public LocalSessionFactoryBean sessionFactory() {
             LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
@@ -28,23 +28,16 @@ public class HibernateConf {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.hsqldb.jdbc.JDBCDriver");
-        dataSource.setUrl("jdbc:hsqldb:file:HSQLDB/testdb");
+        dataSource.setUrl("jdbc:hsqldb:file:HSQLDB/testdb;shutdown=true;hsqldb.write_delay=false;");
         dataSource.setUsername("sa");
         dataSource.setPassword("sa");
 
         return dataSource;
     }
 
-    @Bean
-    public PlatformTransactionManager hibernateTransactionManager() {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory().getObject());
-        return transactionManager;
-    }
-
     private final Properties hibernateProperties() {
         Properties hibernateProperties = new Properties();
-        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
         hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
 
         return hibernateProperties;
